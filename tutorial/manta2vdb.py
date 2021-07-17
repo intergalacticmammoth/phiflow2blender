1b06caa537b53c9c2000bb1d8e5e7433c4f54961
intergalactic-mammoth <aristotelis.economides@gmail.com>
2021-07-17 17:30:50 +0200

    bugfix in manta2vdb

--- tutorial/manta2vdb.py
+++ tutorial/manta2vdb.py
@@ -1,13 +1,9 @@
 import os
 from argparse import ArgumentParser
 import numpy as np
-from phi.flow import *
-from manta  import *
+from pathlib import Path
 
-########################
-# ----Argument Parser----
-########################
-
+# parse args
 parser = ArgumentParser()
 parser.add_argument(
     "-d", "--directory", required=True, help="input directory for processing"
@@ -19,63 +15,37 @@ parser.add_argument(
     required=True,
     help="X Y Z resolution of the phiflow simulation",
 )
-parser.add_argument(
-    "-o", "--output", required=True, help="output directory for vdb files"
-)
 
 args = parser.parse_args()
 
 
-########################
-# ----Mantaflow Setup----
-########################
-
-params = {
-    "dim": 3,
-    "len": 100,
-}
-
+#initialize mantaflow fields
 res = [int(x) for x in args.resolution]
 
 if len(res) != 3:
     raise ValueError("Expected 3 resolution values: X Y Z")
 
 gs = [res[2], res[1], res[0]]
-s = FluidSolver(name="low-res", gridSize=vec3(gs[2], gs[1], gs[0]), dim=params["dim"])
+s = FluidSolver(name="low-res", gridSize=vec3(gs[2], gs[1], gs[0]), dim=3)
 
 # prepare grids
-gF = s.create(FlagGrid)
 gV = s.create(MACGrid)
 gD = s.create(RealGrid)
 vort = s.create(VecGrid)
 vortn = s.create(RealGrid)
 
-gF.initDomain()
-gF.fillGrid()
+# input / output
+input_dir = Path(args.directory)
 
-
-######################
-# ----Output Setup-----
-######################
-
-input_dir = args.directory
-output_dir = input_dir + "post_process/"
-
-if not os.path.exists(input_dir):
+if not input_dir.exists():
     raise ValueError(f"{input_dir} does not exist, choose a sim that exists.")
 
-if args.output is not None:
-    output_dir = args.output + "/"
+output_dir = input_dir/'to_vdb/'
+output_dir.mkdir(parents=True, exist_ok=True)
 
-save_dir = output_dir + "data/"
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
 
-################################
-# ---- Save .npz to OpenVDB format ----
-################################
 
-path = input_dir
+# Find all smoke and velocity files in input directory
 vel_files = []
 smoke_files = []
 
@@ -88,12 +58,12 @@ for dirpath, dirnames, filenames in os.walk(input_dir):
             if "smoke" in file:
                 smoke_files.append(full_data_file)
 
-for fn in vel_files:
-    print("Reading " + fn)
-    a = np.load(fn)["data"]
-    b = a[:-1:, :-1, :-1, ::]  # crop, and flip XYZ vel channels again!
+# convert velocity to vorticity and save to OpenVDB
+for file_name in vel_files:
+    data = np.load(file_name)["data"]
+    data = data[:-1:, :-1, :-1, ::]  # crop, and flip XYZ vel channels again!
 
-    copyArrayToGridMAC(target=gV, source=b)
+    copyArrayToGridMAC(target=gV, source=data)
     computeVorticity(gV, vort, vortn)
 
     vn = np.zeros(shape=(gs[0], gs[1], gs[2], 1), dtype=np.float32)
@@ -101,6 +71,15 @@ for fn in vel_files:
     copyArrayToGridReal(target=vortn, source=vn)
 
     # write vdb file
-    vortn.save(save_dir + f"{fn.split('.')[0]}.vdb")
+    vortn.save(output_dir.__str__() + f"/{file_name.split('.')[0].rsplit('/')[-1]}.vdb")
 
-print(f"\n\nThe .vdb files are saved in {save_dir}.")
+# save smoke to OpenVDB
+for file_name in smoke_files:
+    data = np.load(file_name)["data"]
+
+    copyArrayToGridReal(target=gD, source=data)
+    gD.save(output_dir.__str__() + f"/{file_name.split('.')[0].rsplit('/')[-1]}.vdb")
+    
+
+
+print(f"\n\nThe files are saved in:\n {output_dir}")
